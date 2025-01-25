4d91b6f8fe6063f6d4b2db45a920481b0a46f8cb
tunatuna1733 <bouningen13@gmail.com>
2025-01-25 14:07:27 +0900

    added tests

--- test/rbs/param_const_test.rb
+++ test/rbs/param_const_test.rb
@@ -58,4 +58,124 @@ class Foo
 end
     SIG
   end
+
+  def test_param_const_6
+    assert_writer <<-SIG
+class Foo
+  def foo: (const bool, Integer) -> String
+end
+    SIG
+  end
+
+  def test_param_const_7
+    assert_writer <<-SIG
+class Foo
+  def foo: (bool, const Integer) -> String
+end
+    SIG
+  end
+
+  def test_param_const_8
+    assert_writer <<-SIG
+class Foo
+  def foo: (const (Integer | String), bool) -> String
+end
+    SIG
+  end
+
+  def test_param_const_9
+    assert_writer <<-SIG
+class Foo
+  def foo: (const (Integer | bool), const String) -> String
+end
+    SIG
+  end
+
+  def test_param_const_10
+    assert_writer <<-SIG
+class Foo
+  def foo: (bool, const (Integer & String)) -> String
+end
+    SIG
+  end
+
+  def test_param_const_11
+    assert_writer <<-SIG
+class Foo
+  def foo: (const bool, const String) -> String
+end
+    SIG
+  end
+
+  def test_param_const_12
+    assert_writer <<-SIG
+class Foo
+  def foo: (const Integer, const Foo) -> String
+end
+    SIG
+  end
+
+  def test_param_const_13
+    assert_writer <<-SIG
+class Foo
+  def foo: (const (Integer | String), const (Intger | bool)) -> String
+end
+    SIG
+  end
+
+  def test_param_const_14
+    assert_writer <<-SIG
+class Foo
+  def foo: (const (Integer | bool), const (Integer | String)) -> String
+end
+    SIG
+  end
+
+  def test_param_const_15
+    assert_writer <<-SIG
+class Foo
+  def foo: (const (Integer & String), const Foo) -> String
+end
+    SIG
+  end
+
+  def test_param_const_16
+    assert_writer <<-SIG
+class Foo
+  def foo: (const bool, Integer, const String) -> String
+end
+    SIG
+  end
+
+  def test_param_const_17
+    assert_writer <<-SIG
+class Foo
+  def foo: (bool, const Integer, String) -> String
+end
+    SIG
+  end
+
+  def test_param_const_18
+    assert_writer <<-SIG
+class Foo
+  def foo: (const (Integer | String), bool, const (Integer & Foo)) -> String
+end
+    SIG
+  end
+
+  def test_param_const_19
+    assert_writer <<-SIG
+class Foo
+  def foo: (const (Integer | bool), const String, bool) -> String
+end
+    SIG
+  end
+
+  def test_param_const_20
+    assert_writer <<-SIG
+class Foo
+  def foo: (bool, const (Integer & String), const (String | Foo)) -> String
+end
+    SIG
+  end
 end
